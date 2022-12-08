c470e148973e8023ea2f552a5b33248947e42801
Preina Mariya <m.preina@idaproject.com>
2022-12-08 21:45:00 +0400

    feat: updated nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -36,8 +36,11 @@ export default {
         rel: 'stylesheet',
         href: 'https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap',
       },
-
     ]
+  },
+
+  router: {
+    base: '/april/'
   },
 
   // Global environments
